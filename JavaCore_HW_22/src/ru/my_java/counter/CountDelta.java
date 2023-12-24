8c62b673e3d899dd719b1135f75ae00760de5006
alex123 <alex123@dot.com>
2023-12-24 20:01:58 +0500

    Дозагрузка в комит

--- JavaCore_HW_22/src/ru/my_java/counter/CountDelta.java
+++ JavaCore_HW_22/src/ru/my_java/counter/CountDelta.java
@@ -1,2 +1,27 @@
-package ru.my_java.counter;public class CountDelta {
+package ru.my_java.counter;
+
+import ru.my_java.printer.PrintResult;
+
+import java.util.Arrays;
+
+public class CountDelta {
+
+    private final int[] array;
+    public CountDelta(int[] array){
+        this.array = array;
+    }
+
+    private int countDelta(){
+        int max_value = Arrays.stream(array).max().getAsInt();
+        int min_value = Arrays.stream(array).min().getAsInt();
+        return max_value - min_value;
+    }
+
+    public void showResult(){
+        PrintResult print_result = new PrintResult();
+        String result = Integer.toString(countDelta());
+        String message = "Разница между самым большим и самым маленьким элементами";
+        print_result.printResult(result, message);
+    }
+
 }
--- JavaCore_HW_22/src/ru/my_java/printer/PrintResult.java
+++ JavaCore_HW_22/src/ru/my_java/printer/PrintResult.java
@@ -1,2 +1,18 @@
-package ru.my_java.printer;public class PrintResult {
+package ru.my_java.printer;
+
+public class PrintResult {
+
+    public PrintResult(){
+
+    }
+
+    public void printResult(String result, String message){
+        StringBuilder sb = new StringBuilder();
+        sb.append(message);
+        sb.append(" -> ");
+        sb.append(result);
+        sb.append("\n");
+        System.out.println(sb);
+
+    }
 }
